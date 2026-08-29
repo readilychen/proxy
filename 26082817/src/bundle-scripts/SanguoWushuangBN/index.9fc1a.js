61d588dd67c0415a16b2aee65b9d326c75cd9211
readilychen <readilychen@foxmail.com>
2026-08-29 17:58:59 +0800

    add bundles.zip for version 26082817

---
+++ 26082817/src/bundle-scripts/SanguoWushuangBN/index.9fc1a.js
@@ -0,0 +1,20 @@
+System.register("chunks:///_virtual/SanguoWushuangBN",[],(function(){return{execute:function(){}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/SanguoWushuangBN', 'chunks:///_virtual/SanguoWushuangBN'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
